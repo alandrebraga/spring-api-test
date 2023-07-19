977c09ffef0254f1f04671795e2940cf8a1f75e2
Andre Luiz Mallmann <andre.mallmann@gmail.com>
2023-07-18 21:26:33 -0300

    fix data loader

--- src/main/java/com/abc/contentcalendar/repository/ContentRepository.java
+++ src/main/java/com/abc/contentcalendar/repository/ContentRepository.java
@@ -5,18 +5,11 @@ import com.abc.contentcalendar.model.Status;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface ContentRepository extends ListCrudRepository<Content, Integer> {
 
-    List<Content> findAllByTitleContains(String keyword);
+    List<Content> findAllByContentType(String type);
 
-    @Query("""
-            SELECT * 
-            FROM content
-            WHERE status = :status
-            """)
-    List<Content> listByStatus(@Param("status") Status status);
 }
